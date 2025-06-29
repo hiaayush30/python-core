d83b842ea8f6ab44bd990e766747a088de41666b
aayush <hiaayush30@gmail.com>
2025-06-29 22:36:21 +0530

    lists

---
+++ 05_data_structures/lists.py
@@ -0,0 +1,15 @@
+letters = ["a","b","c"]
+print([0]*5)
+
+combined = [0]*5 + letters
+print(combined)
+
+print([0,1,2,3]*2)
+
+list_from_range= list(range(20))
+print(list_from_range)
+
+chars = list("Hello")
+print(chars)
+
+print(len(list_from_range))
